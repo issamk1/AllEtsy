feb2978e8c57be482bdd08bb78d4207d5f2e1fa8
issamk1 <issam.a.khoury@gmail.com>
2016-09-30 16:30:01 -0400

    Create hidden.py

---
+++ hidden.py
@@ -0,0 +1,8 @@
+# Keep this file separate
+# https://apps.twitter.com/
+
+def oauth() :
+    return { "consumer_key" : "INSERT YOUT API KEY HERE",
+        "consumer_secret" : "INSERT YOUR KEY HERE",
+        "token_key" : "INSERT YOUR KEY HERE",
+        "token_secret" : "INSERT YOUR KEY HERE" }
